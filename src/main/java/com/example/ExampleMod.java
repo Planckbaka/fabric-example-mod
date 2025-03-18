a36e99cb33bdb80c45f86652964cb109e5f7e9df
Planckbaka <akiwayne24@gmail.com>
2025-03-18 21:50:36 +0800

    初步部署了minecraft 1.21.4的fabric的开发环境，并新建了一个物品

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -2,8 +2,15 @@ package com.example;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.function.Function;
 
 public class ExampleMod implements ModInitializer {
 	public static final String MOD_ID = "modid";
@@ -12,7 +19,12 @@ public class ExampleMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Item CUSTOM_ITEM = register("custom_item", Item::new, new Item.Settings());
 
+	public static Item register(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
+		final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("tutorial", path));
+		return Items.register(registryKey, factory, settings);
+	}
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
